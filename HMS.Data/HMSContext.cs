c85f65373808582095bf4fa0236c7f80fab35df5
Ghalib <ghalibs911@gmail.com>
2021-03-31 18:09:14 +0500

    Added Context to Create Table

---
+++ HMS.Data/HMSContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using HMS.Entities;
+
+namespace HMS.Data
+
+{
+    public class HMSContext : DbContext
+    {
+        public HMSContext() : base("HMSConnectionString")
+        {
+        }
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
