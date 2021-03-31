f5d5b2969cd8c300fece1f2f096c3414d99aeda6
Ghalib <ghalibs911@gmail.com>
2021-03-31 18:16:50 +0500

    Creating Migrations

---
+++ HMS.Data/Migrations/202103311315578_BasicEntities.cs
@@ -0,0 +1,29 @@
+namespace HMS.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BasicEntities : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.Users");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        idUser = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        LastName = c.String(nullable: false, maxLength: 50),
+                        Email = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.idUser);
+            
+        }
+    }
+}
