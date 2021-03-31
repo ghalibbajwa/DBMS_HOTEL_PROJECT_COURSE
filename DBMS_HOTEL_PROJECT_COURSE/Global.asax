fe4c6ecbfae7c83b02725fe9b260df5819afcefb
Ghalib <ghalibs911@gmail.com>
2021-03-31 18:05:31 +0500

    Add project files.

---
+++ DBMS_HOTEL_PROJECT_COURSE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DBMS_HOTEL_PROJECT_COURSE.MvcApplication" Language="C#" %>
---
+++ DBMS_HOTEL_PROJECT_COURSE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DBMS_HOTEL_PROJECT_COURSE.Startup))]
+namespace DBMS_HOTEL_PROJECT_COURSE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
