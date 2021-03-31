bcee8785ebfb2aa8416ae28b1f960c72b9982c67
Ghalib <ghalibs911@gmail.com>
2021-03-31 20:36:22 +0500

    Created AccTypes View and Created a Class Library for Services

---
+++ DBMS_HOTEL_PROJECT_COURSE/Areas/Dashboard/Controllers/AccomodationTypesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBMS_HOTEL_PROJECT_COURSE.Areas.Dashboard.Controllers
+{
+    public class AccomodationTypesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       public ActionResult Listing()
+        {
+            //var accomodationTypes = 
+
+            return PartialView("_Listing");
+        }
+    }
+}
